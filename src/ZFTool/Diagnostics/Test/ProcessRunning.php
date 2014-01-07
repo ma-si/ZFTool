273c6c341cf073ddddaa2107761295aafb796ed1
MaSi <webdeveloper@mateuszsitek.com>
2014-01-07 23:48:31 +0100

    applied @Ocramius indications

--- src/ZFTool/Diagnostics/Test/ProcessRunning.php
+++ src/ZFTool/Diagnostics/Test/ProcessRunning.php
@@ -7,49 +7,51 @@ use ZFTool\Diagnostics\Result\Success;
 
 /**
  * Check if system service is running.
- *
- * @package ZFTool\Diagnostics\Test
  */
 class ProcessRunning extends AbstractTest implements TestInterface
 {
     /**
-     * @var string
+     * @var string|null
      */
     private $processName;
+    
+    /**
+     * @var integer|null
+     */
     private $pid;
+    
+    /**
+     * @var string
+     */
     private $os;
 
     /**
      * @param string|int $processNameOrPid   Name or ID of the process to find.
-     * @throws \InvalidArgumentException
+     * @throws ZFTool\Diagnostics\Exception\InvalidArgumentException
      */
     public function __construct($processNameOrPid)
     {
         if (empty($processNameOrPid)) {
-            throw new InvalidArgumentException(sprintf(
+            throw new InvalidArgumentException(
                 'Wrong argument provided for ProcessRunning check - ' .
-                'expected a process name (string) or pid (positive number).',
-                gettype($processNameOrPid)
-            ));
+                'expected a process name (string) or pid (positive number).'
+            );
         }
 
-        if (!is_numeric($processNameOrPid) && !is_scalar($processNameOrPid)) {
+        if (
+            !is_numeric($processNameOrPid) && !is_scalar($processNameOrPid)
+            || (is_numeric($processNameOrPid) && $processNameOrPid < 0)
+        ) {
             throw new InvalidArgumentException(sprintf(
                 'Wrong argument provided for ProcessRunning check - ' .
                 'expected a process name (string) or pid (positive number) but got %s',
-                gettype($processNameOrPid)
+                gettype($processNameOrPid).(is_numeric($processNameOrPid) ? ' '
+                    .(int)$processNameOrPid : '').'.'
             ));
         }
 
         if (is_numeric($processNameOrPid)) {
-            if ((int)$processNameOrPid < 0) {
-                throw new InvalidArgumentException(sprintf(
-                    'Wrong argument provided for ProcessRunning check - ' .
-                    'expected pid to be a positive number but got %s',
-                    (int)$processNameOrPid
-                ));
-            }
-            $this->pid = (int)$processNameOrPid;
+            $this->pid = $processNameOrPid;
         } else {
             $this->processName = $processNameOrPid;
         }
@@ -67,7 +69,8 @@ class ProcessRunning extends AbstractTest implements TestInterface
     }
 
     /**
-     * Run system services check and return a Success if the service were found, Failure if not.
+     * Run system services check and return a Success if the service were found,
+     * Failure if not.
      *
      * @return Failure|Success
      */
@@ -75,35 +78,49 @@ class ProcessRunning extends AbstractTest implements TestInterface
     {
         // TODO: make more OS agnostic
         if ($this->pid) {
-            if($this->os=='windows') {
-                exec('tasklist /svc /fi "pid eq ' . (int)$this->pid . '" | find /i "' . (int)$this->pid . '"', $output, $return);
-            }
-            else {
-                exec('ps -p ' . (int)$this->pid, $output, $return);
-            }
-            
-            if ($return == 1) {
-                return new Failure(sprintf('Process with PID %s is not currently running.', $this->pid));
-            }
-            else
-            {
-                return new Success(sprintf('Process with PID %s is running.', $this->pid));
-            }
+            return $this->checkPid();
         } else {
-            if($this->os=='windows') {
-                exec('sc query ' . escapeshellarg($this->processName) . ' | find "STATE" | find "RUNNING"', $output, $return);
-            }
-            else {
-                exec('ps -ef | grep ' . escapeshellarg($this->processName) . ' | grep -v grep', $output, $return);
-            }
-
-            if ($return == 1) {
-                return new Failure(sprintf('Could not find any running process containing "%s"', $this->processName));
-            }
-            else
-            {
-                return new Success(sprintf('Process %s is running.', $this->processName));
-            }
+            return $this->checkProcessName();
         }
+    }
+    
+    private function checkPid()
+    {
+        if($this->os=='windows') {
+            exec('tasklist /svc /fi "pid eq ' . (int)$this->pid . '" | find /i "'
+                . (int)$this->pid . '"', $output, $return);
+        } else {
+            exec('ps -p ' . (int)$this->pid, $output, $return);
+        }
+
+        if ($return == 1) {
+            return new Failure(
+                sprintf('Process with PID %s is not currently running.', $this->pid)
+            );
+        }
+        
+        return new Success(sprintf('Process with PID %s is running.', $this->pid));
+    }
+    
+    private function checkProcessName()
+    {
+        if($this->os=='windows') {
+            exec('sc query ' . escapeshellarg($this->processName)
+                . ' | find "STATE" | find "RUNNING"', $output, $return);
+        } else {
+            exec('ps -ef | grep ' . escapeshellarg($this->processName)
+                . ' | grep -v grep', $output, $return);
+        }
+
+        if ($return == 1) {
+            return new Failure(
+                sprintf(
+                    'Could not find any running process containing "%s"',
+                    $this->processName
+                )
+            );
+        }
+        
+        return new Success(sprintf('Process %s is running.', $this->processName));
     }
 }
